1484bb12f5e04879a53aaa8fe7e30f1bc85b6df0
_AngieMora1 <angiemorarosas@gmail.com>
2023-07-17 12:40:05 -0500

    archivo almacenamiento de la api

---
+++ src/api/api.js
@@ -0,0 +1,12 @@
+const dataApi = {
+
+}
+const options = {
+    method: 'GET',
+    headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0NTdiYTkyNzI1ZDM1Yzg5NjhmM2UzYTViYzhiNGRhMSIsInN1YiI6IjY0OTRiOTlmYTI4NGViMDBlMmI2OTAzYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ad_6X7S2yTpjQTJg8Iz4dKyjtj6hAOx_LcWXRdpNe8g'
+    }
+    };
+
+export default { dataApi, options }
